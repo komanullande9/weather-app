7bb9a389e40401d3e80cbce0dadfe445d5093cca
komanullande <pwfcujezfz398@hotmail.com>
2025-12-24 11:36:44 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,1 @@
+const express = require('express'); const app = express(); app.get('/', (req, res) => { res.send('Welcome to the Weather App!'); }); app.listen(3000, () => { console.log('Server is running on port 3000'); });
